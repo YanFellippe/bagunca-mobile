b06c384f04a65274f4adfc2bfcd00b65438752a6
Yan Basílio <yangomesbasilio@gmail.com>
2025-04-04 08:43:02 -0300

    conexão entre páginas e banco de dados

---
+++ src/bagunca/Bagunca.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bagunca;
+
+/**
+ *
+ * @author Ruan54254366
+ */
+public class Bagunca {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
